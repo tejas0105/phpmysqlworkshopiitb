7ef341b5c633f8eee599b777c917dc3c118701f2
Tejas Pathak <54174816+tejas0105@users.noreply.github.com>
2020-10-03 14:38:48 +0530

    first commit

--- Day1/q1_1_evenodd.php
+++ Day1/q1_1_evenodd.php
@@ -1,6 +1,4 @@
-<!-- /* Create a page evenodd.php into root directory of your local web server. Here, write a script for
-finding out whether it is an odd number or even number and echo on the screen. The following
-numbers set can be taken as input: 11, 23, 72 and 44.*/ -->
+
 <?php
     $no=11;
     if($no%2==0)
