d55aeb572f53619afa556d175f2125ee34346cea
Tejas Pathak <54174816+tejas0105@users.noreply.github.com>
2020-10-03 14:39:39 +0530

    first commit

--- Day1/q2_1_operators.php
+++ Day1/q2_1_operators.php
@@ -1,6 +1,4 @@
-<!-- Write a PHP Script to declare 2 variables with specific numeric value of your choice and Perform
-the following operations: Addition, Subtraction, Multiplication and Division. Also echo out the
-Result on the screen. -->
+
 <?php
     $n1=20;
     $n2=10;
