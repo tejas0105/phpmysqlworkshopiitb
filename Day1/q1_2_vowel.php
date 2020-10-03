6780da27709fe7962fcce566c84e744caa6e543a
Tejas Pathak <54174816+tejas0105@users.noreply.github.com>
2020-10-03 14:39:16 +0530

    first commit

--- Day1/q1_2_vowel.php
+++ Day1/q1_2_vowel.php
@@ -1,7 +1,4 @@
-<!-- Create a page vowel.php into root directory of your local web server. Write in the script for
-determining whether or not the character associated with a variable is Vowel or a Consonant. If its
-a Vowel echo out the name of the vowel otherwise echo out its a Consonant. Use Switch for this
-script. -->
+
 <?php
     $ab="e";
     $space=" ";
