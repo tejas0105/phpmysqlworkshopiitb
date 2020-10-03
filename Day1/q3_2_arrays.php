a22ddc6e671aa18d25bef23fc4cb8a6831e604d6
Tejas Pathak <54174816+tejas0105@users.noreply.github.com>
2020-10-03 14:40:49 +0530

    first commit

--- Day1/q3_2_arrays.php
+++ Day1/q3_2_arrays.php
@@ -1,4 +1,4 @@
-<!-- Write a PHP script to add the two 2x2 Matrices and output the result. -->
+
 <?php
     $mat=array(array(1,2),array(3,4));
     echo"Element at 1st,1st position ".$mat[0][0]."<br>";
