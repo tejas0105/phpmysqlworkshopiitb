86c2867152aac27c3503e6bcd61bec26229258c5
Tejas Pathak <54174816+tejas0105@users.noreply.github.com>
2020-10-03 18:04:56 +0530

    Third commit

--- Day3/q2.php
+++ Day3/q2.php
@@ -11,10 +11,10 @@ $extract = mysqli_query($connect,"SELECT * FROM class1 WHERE name='Rohan'");
 
 while ($row = mysqli_fetch_assoc($extract)) {
 
-$s1 = $row["sub1"];
-$s2 = $row["sub2"];
-$s3 = $row["sub3"];
-$s4 = $row["sub4"];
+$s1 = $row["s1"];
+$s2 = $row["s2"];
+$s3 = $row["s3"];
+$s4 = $row["s4"];
 $s5 = 99;
 $name1 = $row["name"];
 $newtotal = $s1+$s2+$s3+$s4+$s5;
